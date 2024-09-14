9f01714a24cc1caabf0e88f4710bffeb5010a30f
Nathan Santiago <santiagonathan9254@gmail.com>
2024-09-14 18:06:26 -0400

    Reformat title text

--- vthacks/app/triassic/page.jsx
+++ vthacks/app/triassic/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         {/* Left Section: Title and Description */}
         <div className="carousel carousel-vertical h-[99dvh] w-full flex items-center">
           <div className=" carousel-item h-full w-5/6 flex-col justify-center text-left">
-            <h2 className="text-5xl md:text-7xl font-extrabold bg-gradient-to-r from-green-700 via-lime-500 to-yellow-600 text-transparent bg-clip-text drop-shadow-lg mb-6">
+            <h2 className="text-5xl font-extrabold bg-gradient-to-r from-green-700 via-lime-500 to-yellow-600 text-transparent bg-clip-text drop-shadow-lg mb-6">
               Welcome to the Triassic Period
             </h2>
             <h2 className="text-2xl md:text-4xl font-bold drop-shadow-md mt-4 px-4">
