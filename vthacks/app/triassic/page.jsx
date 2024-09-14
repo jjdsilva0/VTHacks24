42c03c3dc869895d27ba8b2373feabff9a8d6cb5
AlexSieni <asieni@unc.edu>
2024-09-14 17:11:33 -0400

    carousel

--- vthacks/app/triassic/page.jsx
+++ vthacks/app/triassic/page.jsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-base-200 text-base-content px-2 py-4 flex">
+    <div className="carousel carousel-vertical h-[99dvh] w-full bg-base-200 text-base-content px-2 py-4 flex">
       {/* Left Section: Title and Description */}
-      <div className="w-full md:w-3/4 flex items-center">
+      <div className="w-5/6 flex items-center">
         <div className="text-center md:text-left">
           <h2 className="text-5xl md:text-7xl font-extrabold bg-gradient-to-r from-green-700 via-lime-500 to-yellow-600 text-transparent bg-clip-text drop-shadow-lg mb-6">
             Welcome to the Triassic Period
@@ -16,12 +16,8 @@ export default function Home() {
           </h2>
         </div>
       </div>
-
-      {/* Right Section: Dark Grey Box with Rounded Corners */}
-      <div className="hidden md:flex md:w-1/4 bg-gray-800 h-screen rounded-l-lg">
-        <div className="flex justify-center items-center w-full text-white">
-          <h2 className="text-xl">User Input Box</h2>
-        </div>
+      <div className="w-1/6 flex items-center">
+      HERE
       </div>
     </div>
   );
