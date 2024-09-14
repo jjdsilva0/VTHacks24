f435399c0849eb4d740306c2110d9f3ab0bca177
AlexSieni <asieni@unc.edu>
2024-09-14 15:01:39 -0400

    title

--- vthacks/app/page.js
+++ vthacks/app/page.js
@@ -11,9 +11,9 @@ export default function Home() {
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
             <div className="max-w-md">
-              <h1 className="mb-3 text-5xl font-bold">Era Explorer</h1>
+              <h1 className="mb-3 text-5xl font-bold">Dinologue</h1>
               <h2 className="mb-5 text-2xl">
-                Dinologue
+              The ultimate dinosaur knowledge hub.
               </h2>
               <button className="btn btn-primary">Embark</button>
             </div>
