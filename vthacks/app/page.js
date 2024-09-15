16e805198a43a4491fbece58b7761517f4072b77
Joel D'Silva <jjdsilva@unc.edu>
2024-09-15 02:04:17 -0400

    tried to add audio on static elements

--- vthacks/app/page.js
+++ vthacks/app/page.js
@@ -8,17 +8,17 @@ export default function Home() {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(https://images.pexels.com/photos/3839557/pexels-photo-3839557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)",
-          }}>
+            backgroundImage:
+              "url(https://images.pexels.com/photos/3839557/pexels-photo-3839557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)",
+          }}
+        >
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
             <div className="max-w-md">
-            <h1 className="mb-3 text-6xl font-extrabold">
-              Dinologue
-            </h1>
-            <h2 className="mb-5 text-3xl font-semibold text-gray-300 tracking-wider animate-fadeIn">
-              An interactive journey into dinosaur history
-            </h2>
+              <h1 className="mb-3 text-6xl font-extrabold">Dinologue</h1>
+              <h2 className="mb-5 text-3xl font-semibold text-gray-300 tracking-wider animate-fadeIn">
+                An interactive journey into dinosaur history
+              </h2>
 
               <EmbarkButton />
             </div>
@@ -29,15 +29,21 @@ export default function Home() {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(https://i.natgeofe.com/n/5c048a29-390c-4664-8100-01efe5ca01b5/955.jpg?w=1436&h=1078)",
-          }}>
+            backgroundImage:
+              "url(https://i.natgeofe.com/n/5c048a29-390c-4664-8100-01efe5ca01b5/955.jpg?w=1436&h=1078)",
+          }}
+        >
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
             <Link href="/triassic">
               <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
                 <h1 className="mb-3 text-5xl font-bold">Triassic</h1>
                 <p className="mb-5">
-                  The Triassic Period (252-201 million years ago) marked the start of the Mesozoic Era. Following a massive extinction, it saw the rise of early dinosaurs, mammals, and marine reptiles. The climate was hot and dry, with vast deserts. The period ended with a significant extinction event.
+                  The Triassic Period (252-201 million years ago) marked the
+                  start of the Mesozoic Era. Following a massive extinction, it
+                  saw the rise of early dinosaurs, mammals, and marine reptiles.
+                  The climate was hot and dry, with vast deserts. The period
+                  ended with a significant extinction event.
                 </p>
               </div>
             </Link>
@@ -49,15 +55,22 @@ export default function Home() {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(https://i.natgeofe.com/n/5c7fabf3-edbc-4956-af56-15dc547371b7/810.jpg?w=1436&h=1078)",
-          }}>
+            backgroundImage:
+              "url(https://i.natgeofe.com/n/5c7fabf3-edbc-4956-af56-15dc547371b7/810.jpg?w=1436&h=1078)",
+          }}
+        >
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
             <Link href="/jurassic">
               <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
                 <h1 className="mb-3 text-5xl font-bold">Jurassic</h1>
                 <p className="mb-5">
-                  The Jurassic Period (201-145 million years ago) was a time of significant dinosaur dominance and diversification. It saw the rise of iconic species like Stegosaurus and Brachiosaurus. The climate was warm and humid, fostering lush vegetation. Marine life thrived with the presence of large reptiles like plesiosaurs. The period ended with a minor extinction event.
+                  The Jurassic Period (201-145 million years ago) was a time of
+                  significant dinosaur dominance and diversification. It saw the
+                  rise of iconic species like Stegosaurus and Brachiosaurus. The
+                  climate was warm and humid, fostering lush vegetation. Marine
+                  life thrived with the presence of large reptiles like
+                  plesiosaurs. The period ended with a minor extinction event.
                 </p>
               </div>
             </Link>
@@ -69,15 +82,22 @@ export default function Home() {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(https://i.natgeofe.com/n/9c56eafe-8285-4eef-b0d4-e12473098742/907.jpg?w=1436&h=1078)",
-          }}>
+            backgroundImage:
+              "url(https://i.natgeofe.com/n/9c56eafe-8285-4eef-b0d4-e12473098742/907.jpg?w=1436&h=1078)",
+          }}
+        >
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
             <Link href="/cretaceous">
               <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
                 <h1 className="mb-3 text-5xl font-bold">Cretaceous</h1>
                 <p className="mb-5">
-                  The Cretaceous Period (145-66 million years ago) was the final era of the dinosaurs. It saw the rise of famous species like Tyrannosaurus rex and Triceratops. The climate was warm, supporting diverse ecosystems. Flowering plants emerged, and marine life flourished with creatures like mosasaurs. The period ended with a massive extinction event.
+                  The Cretaceous Period (145-66 million years ago) was the final
+                  era of the dinosaurs. It saw the rise of famous species like
+                  Tyrannosaurus rex and Triceratops. The climate was warm,
+                  supporting diverse ecosystems. Flowering plants emerged, and
+                  marine life flourished with creatures like mosasaurs. The
+                  period ended with a massive extinction event.
                 </p>
               </div>
             </Link>
