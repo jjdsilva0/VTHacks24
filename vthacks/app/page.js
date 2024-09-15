544f2832de72963d2d40c1d3d877b04d1f2247dd
Nathan Santiago <santiagonathan9254@gmail.com>
2024-09-14 20:04:13 -0400

    Add links in the homepage.

--- vthacks/app/page.js
+++ vthacks/app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import EmbarkButton from "./components/EmbarkButton";
 
 export default function Home() {
@@ -22,7 +23,6 @@ export default function Home() {
         </div>
       </div>
       <div className="carousel-item h-full" id="triassic">
-        {/* TODO: Create a link to the triassic page. */}
         <div
           className="hero min-h-screen"
           style={{
@@ -30,12 +30,14 @@ export default function Home() {
           }}>
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
-            <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
-              <h1 className="mb-3 text-5xl font-bold">Triassic</h1>
-              <p className="mb-5">
-                The Triassic Period (252-201 million years ago) marked the start of the Mesozoic Era. Following a massive extinction, it saw the rise of early dinosaurs, mammals, and marine reptiles. The climate was hot and dry, with vast deserts. The period ended with another significant extinction event.
-              </p>
-            </div>
+            <Link href="/triassic">
+              <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
+                <h1 className="mb-3 text-5xl font-bold">Triassic</h1>
+                <p className="mb-5">
+                  The Triassic Period (252-201 million years ago) marked the start of the Mesozoic Era. Following a massive extinction, it saw the rise of early dinosaurs, mammals, and marine reptiles. The climate was hot and dry, with vast deserts. The period ended with another significant extinction event.
+                </p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
@@ -48,12 +50,14 @@ export default function Home() {
           }}>
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
-            <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
-              <h1 className="mb-3 text-5xl font-bold">Jurassic</h1>
-              <p className="mb-5">
-                The Jurassic Period (201-145 million years ago) was a time of significant dinosaur dominance and diversification. It saw the rise of iconic species like Stegosaurus and Brachiosaurus. The climate was warm and humid, fostering lush vegetation. Marine life thrived with the presence of large reptiles like plesiosaurs. The period ended with a minor extinction event.
-              </p>
-            </div>
+            <Link href="/jurassic">
+              <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
+                <h1 className="mb-3 text-5xl font-bold">Jurassic</h1>
+                <p className="mb-5">
+                  The Jurassic Period (201-145 million years ago) was a time of significant dinosaur dominance and diversification. It saw the rise of iconic species like Stegosaurus and Brachiosaurus. The climate was warm and humid, fostering lush vegetation. Marine life thrived with the presence of large reptiles like plesiosaurs. The period ended with a minor extinction event.
+                </p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
@@ -66,12 +70,14 @@ export default function Home() {
           }}>
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
-            <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
-              <h1 className="mb-3 text-5xl font-bold">cretaceous</h1>
-              <p className="mb-5">
-                The Cretaceous Period (145-66 million years ago) was the final era of the dinosaurs. It saw the rise of famous species like Tyrannosaurus rex and Triceratops. The climate was warm, supporting diverse ecosystems. Flowering plants emerged, and marine life flourished with creatures like mosasaurs. The period ended with a massive extinction event.
-              </p>
-            </div>
+            <Link href="/cretaceous">
+              <div className="max-w-md text-left bg-black bg-opacity-40 p-5 rounded-lg">
+                <h1 className="mb-3 text-5xl font-bold">Cretaceous</h1>
+                <p className="mb-5">
+                  The Cretaceous Period (145-66 million years ago) was the final era of the dinosaurs. It saw the rise of famous species like Tyrannosaurus rex and Triceratops. The climate was warm, supporting diverse ecosystems. Flowering plants emerged, and marine life flourished with creatures like mosasaurs. The period ended with a massive extinction event.
+                </p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
