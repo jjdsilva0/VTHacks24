062d63a994bd22d85e742b0a37d90e5cd0113422
AlexSieni <asieni@unc.edu>
2024-09-14 23:24:17 -0400

    main page styling

--- vthacks/app/page.js
+++ vthacks/app/page.js
@@ -13,10 +13,13 @@ export default function Home() {
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-neutral-content text-center">
             <div className="max-w-md">
-              <h1 className="mb-3 text-5xl font-bold">Dinologue</h1>
-              <h2 className="mb-5 text-2xl">
+            <h1 className="mb-3 text-6xl font-extrabold">
+              Dinologue
+            </h1>
+            <h2 className="mb-5 text-3xl font-semibold text-gray-300 tracking-wider animate-fadeIn">
               An interactive journey into dinosaur history
-              </h2>
+            </h2>
+
               <EmbarkButton />
             </div>
           </div>
